8878e00da841a721bc5c86792d1fb71b951173a2
Murilex01 <murilo.sxv@gmail.com>
2024-09-22 09:09:29 -0300

    commit 1
    Problemas na conexão com o CSS

--- library-backend/models/book.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const bookSchema = new mongoose.Schema({
-    tilte: {type: String, required: true},
-    author: {type: String, required: true},
-    year: {type: Number},
-})
-
-module.exports = mongoose.model ('Book', BookSchema);
